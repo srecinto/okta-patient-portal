1d692cfbc23981155d23d039eabe15bb870faa2b
= <=>
2020-03-30 19:24:43 +0000

    change app reference to application for aws elastic beastalk to work

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 import os
-from oktapatientportal import app
+from oktapatientportal import app as application
 
 
 """
@@ -7,4 +7,4 @@ MAIN ###########################################################################
 """
 if __name__ == "__main__":
     # This is to run on c9.io.. you may need to change or make your own runner
-    app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8080)))
+    application.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8080)))
